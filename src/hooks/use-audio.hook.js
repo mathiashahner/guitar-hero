4b10f1c02253b2a56d4b268649a555a3bcfaffd8
Mathias Hahner <mathias.hahner@cwi.com.br>
2024-01-02 10:57:34 -0300

    Monitoring end of audio

--- src/hooks/use-audio.hook.js
+++ src/hooks/use-audio.hook.js
@@ -1,14 +1,31 @@
 import { useEffect, useState } from 'react'
+import { useGlobalGame } from '../contexts'
 
 export const useAudio = url => {
   const [audio] = useState(new Audio(url))
   const [playing, setPlaying] = useState(false)
+  const [globalGame, setGlobalGame] = useGlobalGame()
 
-  const togglePlay = () => setPlaying(!playing)
+  const togglePlay = () => {
+    setPlaying(!playing)
+  }
+
+  const onEnded = () => {
+    setGlobalGame({ ...globalGame, gameOver: true })
+    setPlaying(false)
+  }
 
   useEffect(() => {
     playing ? audio.play() : audio.pause()
   }, [playing])
+
+  useEffect(() => {
+    audio.addEventListener('ended', onEnded)
+
+    return () => {
+      audio.removeEventListener('ended', onEnded)
+    }
+  }, [])
 
   return [playing, togglePlay, audio]
 }
